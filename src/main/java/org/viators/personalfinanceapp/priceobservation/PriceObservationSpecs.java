3ed4f0004320b779e7f1943541205a725e334be0
panosV <panosVasilopoulos92@gmail.com>
2026-02-20 17:50:45 +0200

    feat(PFA-36): Creates Specifications for dynamic queries.

--- src/main/java/org/viators/personalfinanceapp/priceobservation/PriceObservationSpecs.java
+++ src/main/java/org/viators/personalfinanceapp/priceobservation/PriceObservationSpecs.java
@@ -22,4 +22,25 @@ public class PriceObservationSpecs {
         return ((root, query, cb) ->
                 cb.lessThanOrEqualTo(root.get("observationDate"), dateTo));
     }
+
+    public static Specification<PriceObservation> hasStoreUuid(String storeUuid) {
+        return (root, query, cb) ->
+                cb.equal(root.get("store").get("uuid"), storeUuid);
+    }
+
+    public static Specification<PriceObservation> hasStoreType(StoreTypeEnum storeType) {
+        return (root, query, cb) ->
+                cb.equal(root.get("store").get("storeType"), storeType);
+    }
+
+    public static Specification<PriceObservation> inCity(String city) {
+        return (root, query, cb) ->
+                cb.equal(root.get("store").get("city"), city);
+    }
+
+    public static Specification<PriceObservation> hasCurrency(CurrencyEnum currency) {
+        return (root, query, cb) ->
+                cb.equal(root.get("currency"), currency);
+    }
+
 }
